8e609b7883cf2156d04d0399b5a2d1ca049f2049
Muh. Yasin Ardhi Sawal <muh.yasin.ardhi.sawal@gmail.com>
2020-06-13 19:45:28 +0800

    Updates

---
+++ precache-manifest.66277f06736548391e808f195a3ec182.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "722b2745b70ea16981fd16cb403bc6b5",
+    "url": "/dad-jokes-app/index.html"
+  },
+  {
+    "revision": "10397a4cb451acefee7e",
+    "url": "/dad-jokes-app/static/css/main.4c6f5cef.chunk.css"
+  },
+  {
+    "revision": "3828315cee67241b4f89",
+    "url": "/dad-jokes-app/static/js/2.624797d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dad-jokes-app/static/js/2.624797d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10397a4cb451acefee7e",
+    "url": "/dad-jokes-app/static/js/main.d368909b.chunk.js"
+  },
+  {
+    "revision": "a9e1c9a081cec1f4fd54",
+    "url": "/dad-jokes-app/static/js/runtime-main.2a255ce6.js"
+  }
+]);
